8e98ac40878fd8076e71258cb63f4baae2cfa28b
MilkshakePanda <iam.superpeluche@gmail.com>
2015-08-31 11:24:00 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Milkshake Panda Starter Kit
+
+Milky is a very basic boilerplate for simple front end projects.
+
+Opinionated (Pure sass, bourbon, neat, no compass)
+
+It has Sass, Jade, Bourbon and Neat built-in to it. A Jekyll ready version is on the works as well and should be out soon.
+
+All you have to do is browse to the /dev folder and run (sudo) npm install and then gulp.
+
+gulp will run all the necessary tasks and will watch any changes you make to the development files.
+
+Everything will be outputed to the root folder.
+
+I suck at writing documentation so feel free to correct me if you stumble upon this and find this documentation awful.
+
+Have fun coding :)
---
+++ dev/gulpfile.js
@@ -0,0 +1,105 @@
+var gulp = require('gulp');
+var imagemin = require('gulp-imagemin');
+var sass = require('gulp-ruby-sass');
+var jade = require('gulp-jade');
+var uglify = require('gulp-uglify');
+var prefix = require('gulp-autoprefixer');
+var browserSync = require('browser-sync');
+var plumber = require('gulp-plumber');
+var reload = browserSync.reload;
+
+
+// Browser Sync
+
+gulp.task('browser-sync', function(){
+
+   var files = [
+
+      '/jade/**/*.jade',
+      '/js/*.js',
+      '/sass/**/*.sass'
+
+   ];
+
+   browserSync(files, {
+
+      server: '../',
+      notify: true
+
+   });
+
+});
+
+
+// Jade
+
+gulp.task('jade', function () {
+
+   gulp.src('./jade/*.jade')
+      .pipe(jade({
+         pretty: true
+      }))
+      .on('error', console.log)
+      .pipe(gulp.dest('../'))
+      .pipe(reload({stream: true}));
+
+});
+
+
+// Sass + prefix
+
+gulp.task('sass', function(){
+
+   return sass('sass/main.sass', {
+
+      style: 'expanded',
+      loadPath: 'sass'
+
+   })
+   .on('error', function (err) {
+      console.log('Error !', err.message);
+   })
+   .pipe(prefix({
+      browsers: ['last 2 versions', '> 5%']
+   }))
+   .pipe(gulp.dest('../assets/css'))
+   .pipe(reload({stream: true}));
+
+});
+
+// Uglify
+
+gulp.task('uglify', function () {
+
+   gulp.src('js/*.js')
+      .pipe(plumber())
+      .pipe(uglify())
+      .pipe(gulp.dest('../assets/js'))
+      .pipe(reload({stream: true}));
+
+});
+
+// imagemin
+
+gulp.task('imagemin', function(){
+
+   gulp.src('img/*')
+      .pipe(imagemin())
+      .pipe(gulp.dest('../assets/img'));
+
+});
+
+// watch
+
+gulp.task('watch', function () {
+
+   gulp.watch('jade/**/*.jade', ['jade']); // watch the jade files
+   gulp.watch('sass/**/*.sass', ['sass']); // watch the sass files
+   gulp.watch('js/*.js', ['uglify']); // watch the js files
+   gulp.watch('img/*', ['imagemin']); // watch the img files
+
+});
+
+// default
+
+gulp.task('default', ['jade', 'sass', 'uglify', 'imagemin', 'browser-sync', 'watch']);
