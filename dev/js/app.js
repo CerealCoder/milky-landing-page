3ad931dde5634a818fb1a91960ae7d450a10f4a1
MilkshakePanda <iam.superpeluche@gmail.com>
2015-09-10 17:54:04 +0100

    Start scripting setup section

--- dev/js/app.js
+++ dev/js/app.js
@@ -16,3 +16,9 @@
    window.onload = revealSite; // when the content is loaded, wait 2 seconds and trigger the revealing animation
 
 })();
+
+// By default, .showing is set to the first child
+// When you click on the next button, remove the .showing class and add it to the next sibling element
+// .showing will trigger a fade-in animation
+// if there is no next sibling add the class of disabled to the button (pointer-events none blablabla)
+// also the title of the step needs to change (set it as a data-title attribute)
