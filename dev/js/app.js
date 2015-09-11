f4f0b7327f1ad30de17d6b2e5011d4480f6491cf
MilkshakePanda <iam.superpeluche@gmail.com>
2015-09-11 17:48:06 +0100

    Start JS on the setup section

--- dev/js/app.js
+++ dev/js/app.js
@@ -3,7 +3,11 @@
    // cache dom
    var body = document.body;
    var logo = document.querySelector('.loading-wrapper span');
-
+   // title
+   // next button
+   // prev button
+   // showing code element
+   
    var revealSite = setTimeout(function() {
 
       logo.classList.remove('pulse-scale'); // remove the pulse-scale class
@@ -12,13 +16,19 @@
 
    }, 2000);
 
+   // Updates the title
+   var updateTitle = function() {
+      // title inner html = showing data attribute
+
+   }
+
+   // By default, .showing is set to the first child
+   // When you click on the next button, remove the .showing class and add it to the next sibling element
+   // .showing will trigger a fade-in animation
+   // if there is no next sibling add the class of disabled to the button (pointer-events none blablabla)
+   // also the title of the step needs to change (set it as a data-title attribute)
+
 
    window.onload = revealSite; // when the content is loaded, wait 2 seconds and trigger the revealing animation
 
 })();
-
-// By default, .showing is set to the first child
-// When you click on the next button, remove the .showing class and add it to the next sibling element
-// .showing will trigger a fade-in animation
-// if there is no next sibling add the class of disabled to the button (pointer-events none blablabla)
-// also the title of the step needs to change (set it as a data-title attribute)
--- notes.md
+++ notes.md
@@ -1,8 +1,8 @@
 ## Todo
-- get the syntax highlighting right
-- create the code for the different phases
-- create the sub nav (prev next ? pagination ? decide)
-- create the animations for the pagination buttons
+
+- Create svg folder and change the include paths
 - Javascript functionality on the setup section
 - Javscript scroll animations
 - Add copy
+- unhide the loading-wrapper
+- link to app.js not the dev file please
